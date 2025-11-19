5fe2e44dbdfbf765ca5ad35bd06e20e997521227
boitenge <jeromeboitenge@gmail.com>
2025-11-19 10:46:20 +0200

    removing comments in page

--- app/page.tsx
+++ app/page.tsx
@@ -10,32 +10,22 @@ import { motion } from 'framer-motion';
 export default function HomePage() {
   return (
     <main className="scroll-smooth">
-      {/* Hero / Home Section */}
       <section id="home">
         <Hero />
       </section>
-
-      {/* About Section */}
       <section id="about">
         <About />
       </section>
-
-      {/* Skills Section */}
       <section id="skills">
         <Skills />
       </section>
 
-      {/* Projects Section */}
       <section id="projects">
         <Projects />
       </section>
-
-      {/* Experience Section */}
       <section id="experience">
         <Experience />
       </section>
-
-      {/* Contact Section */}
       <section id="contact">
         <Contact />
       </section>
