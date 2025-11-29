ea89d480f0ee6a31aba2b6399802c111d3d0c2bd
boitenge <jeromeboitenge@gmail.com>
2025-11-29 21:15:16 +0200

    f

--- app/components/SkillCard.tsx
+++ app/components/SkillCard.tsx
@@ -8,7 +8,7 @@ export default function SkillCard({ name }: { name: string }) {
       whileHover={{ scale: 1.1 }}
       className="px-4 py-2 bg-lightBg dark:bg-darkText text-darkText dark:text-lightBg rounded-full shadow hover:shadow-lg transition cursor-pointer"
     >
-      {name}
+      {name
     </motion.div>
   );
 }
